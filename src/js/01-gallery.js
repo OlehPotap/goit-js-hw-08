d442a8f17b3f51c403ed37d2cc71456a403af918
OlehPotap <potap.oleh.v@gmail.com>
2022-01-15 18:49:51 +0200

    task02

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
+import simpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
+
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+galleryItems.forEach(el => {
+  const newEl = document.createElement('li');
+  newEl.innerHTML = `<a class="gallery__item" href="${el.original}">
+<img
+        class="gallery__image"
+        src="${el.preview}"
+        alt="${el.description}"
+      />
+      </a>
+      `;
+  galleryEl.append(newEl);
+});
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+'use strict';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('#vimeo-player');
+
+const player = new Player(iframe);
+player.setCurrentTime(Number(localStorage.getItem('videoplayer-current-time')));
+
+player.on('timeupdate', function (event) {
+  localStorage.setItem('videoplayer-current-time', event.seconds);
+  console.log(localStorage.getItem('videoplayer-current-time'));
+});

3b56d0f08a705bd8940cfe049b1975c34a75dd85
OlehPotap <potap.oleh.v@gmail.com>
2022-01-16 11:46:38 +0200

    task03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,27 @@
+'use strict';
+import throttle from 'lodash.throttle';
 
+const feedbackFormEl = document.querySelector('.feedback-form');
+const emailAreaEl = document.querySelector('input');
+const textAreaEl = document.querySelector('textarea');
+
+function showEvent(event) {
+  let feedbackFormState = {
+    email: emailAreaEl.value,
+    password: textAreaEl.value,
+  };
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(feedbackFormState));
+}
+
+feedbackFormEl.addEventListener('input', throttle(showEvent, 500, { leading: false }));
+
+emailAreaEl.value = JSON.parse(localStorage.getItem('feedback-form-state')).email;
+textAreaEl.value = JSON.parse(localStorage.getItem('feedback-form-state')).password;
+
+feedbackFormEl.addEventListener('submit', event => {
+  event.preventDefault();
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  feedbackFormEl.reset();
+  localStorage.removeItem('feedback-form-state');
+});

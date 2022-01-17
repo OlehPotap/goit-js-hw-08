b119058503bdd8d0fdbf59836dfc84ccdd495d64
OlehPotap <potap.oleh.v@gmail.com>
2022-01-17 12:42:46 +0200

    task03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,19 +5,29 @@ const feedbackFormEl = document.querySelector('.feedback-form');
 const emailAreaEl = document.querySelector('input');
 const textAreaEl = document.querySelector('textarea');
 
-function showEvent(event) {
-  let feedbackFormState = {
+// ============================Мой способ====================================================
+
+function storeData(event) {
+  const feedbackFormState = {
     email: emailAreaEl.value,
-    password: textAreaEl.value,
+    message: textAreaEl.value,
   };
 
   localStorage.setItem('feedback-form-state', JSON.stringify(feedbackFormState));
 }
 
-feedbackFormEl.addEventListener('input', throttle(showEvent, 500, { leading: false }));
+feedbackFormEl.addEventListener('input', throttle(storeData, 500, { leading: false }));
+
+const CheckFormValues = () => {
+  if (emailAreaEl.value === '' || textAreaEl.value === '') {
+    return;
+  }
+};
+
+CheckFormValues();
 
 emailAreaEl.value = JSON.parse(localStorage.getItem('feedback-form-state')).email;
-textAreaEl.value = JSON.parse(localStorage.getItem('feedback-form-state')).password;
+textAreaEl.value = JSON.parse(localStorage.getItem('feedback-form-state')).message;
 
 feedbackFormEl.addEventListener('submit', event => {
   event.preventDefault();
@@ -25,3 +35,38 @@ feedbackFormEl.addEventListener('submit', event => {
   feedbackFormEl.reset();
   localStorage.removeItem('feedback-form-state');
 });
+
+// =========================Способ как на занятии======================================================
+
+// const feedbackFormState = {};
+
+// feedbackFormEl.addEventListener('input', event => {
+//   const target = event.target;
+
+//   feedbackFormState[target.name] = target.value;
+
+//   localStorage.setItem('feedback-form-state', JSON.stringify(feedbackFormState));
+// });
+
+// feedbackFormEl.addEventListener('submit', event => {
+//   event.preventDefault();
+//   console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+//   feedbackFormEl.reset();
+//   localStorage.removeItem('feedback-form-state');
+// });
+
+// const fillFormFields = () => {
+//   const localStorageFormData = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+//   if (localStorageFormData === null) {
+//     return;
+//   }
+
+//   const keys = Object.keys(localStorageFormData);
+
+//   for (const key of keys) {
+//     feedbackFormEl.elements[key].value = localStorageFormData[key];
+//   }
+// };
+
+// fillFormFields();

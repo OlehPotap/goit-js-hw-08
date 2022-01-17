9cece660ffa3bd43d0757fc35c005df549768315
OlehPotap <potap.oleh.v@gmail.com>
2022-01-17 14:13:41 +0200

    task08

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,29 +5,66 @@ const feedbackFormEl = document.querySelector('.feedback-form');
 const emailAreaEl = document.querySelector('input');
 const textAreaEl = document.querySelector('textarea');
 
-// ============================Мой способ====================================================
+// ============================Мой способ 1 не рабочий====================================================
+
+// function doTheThinOnPage() {
+
+//   if (emailAreaEl.value === "" || textAreaEl.value === "") {
+//     return;
+//   };
+
+//   emailAreaEl.value = JSON.parse(localStorage.getItem('feedback-form-state')).email;
+//   textAreaEl.value = JSON.parse(localStorage.getItem('feedback-form-state')).message;
+
+//   function storeData(event) {
+//     const feedbackFormState = {
+//       email: emailAreaEl.value,
+//       message: textAreaEl.value,
+//     };
+
+//     localStorage.setItem('feedback-form-state', JSON.stringify(feedbackFormState));
+//   }
+
+//   feedbackFormEl.addEventListener('input', throttle(storeData, 500, { leading: false }));
+
+//   feedbackFormEl.addEventListener('submit', event => {
+//     event.preventDefault();
+//     console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+//     feedbackFormEl.reset();
+//     localStorage.removeItem('feedback-form-state');
+//   });
+// }
+// doTheThinOnPage();
+
+// ========================Мой способ 2 рабочий===============================================
+
+const feedbackFormState = {};
+
+// Выводим данные из стореджа если они есть
+
+function fillForm() {
+  const localStorageFormData = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (localStorageFormData === null) {
+    return;
+  }
+
+  emailAreaEl.value = JSON.parse(localStorage.getItem('feedback-form-state')).email;
+  textAreaEl.value = JSON.parse(localStorage.getItem('feedback-form-state')).message;
+}
+
+fillForm();
+// Сохраняем введенные в инпут данные в локальное хранилище
 
 function storeData(event) {
-  const feedbackFormState = {
-    email: emailAreaEl.value,
-    message: textAreaEl.value,
-  };
+  feedbackFormState.email = emailAreaEl.value;
+  feedbackFormState.message = textAreaEl.value;
 
   localStorage.setItem('feedback-form-state', JSON.stringify(feedbackFormState));
 }
 
 feedbackFormEl.addEventListener('input', throttle(storeData, 500, { leading: false }));
 
-const CheckFormValues = () => {
-  if (emailAreaEl.value === '' || textAreaEl.value === '') {
-    return;
-  }
-};
-
-CheckFormValues();
-
-emailAreaEl.value = JSON.parse(localStorage.getItem('feedback-form-state')).email;
-textAreaEl.value = JSON.parse(localStorage.getItem('feedback-form-state')).message;
+// Выполняем действие по слбытию нажатия кнопки
 
 feedbackFormEl.addEventListener('submit', event => {
   event.preventDefault();
